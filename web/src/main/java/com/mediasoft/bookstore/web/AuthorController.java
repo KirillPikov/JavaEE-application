48e2f945688163dac158e3c3021a699d2c70a165
KirillPikov <pikov_kirya@bk.ru>
2020-01-20 19:01:58 +0400

    Update AuthorController.java

--- web/src/main/java/com/mediasoft/bookstore/web/AuthorController.java
+++ web/src/main/java/com/mediasoft/bookstore/web/AuthorController.java
@@ -3,10 +3,13 @@ package com.mediasoft.bookstore.web;
 import com.mediasoft.bookstore.author.ejb.AuthorEjbLocal;
 import com.mediasoft.bookstore.author.entity.Author;
 import com.mediasoft.bookstore.common.loggable.Loggable;
+import com.mediasoft.bookstore.common.pageable.Pageable;
+import com.mediasoft.bookstore.common.pageable.Sort;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.List;
 
 @Named
 @Loggable
@@ -18,8 +21,32 @@ public class AuthorController {
 
     private Author author;
 
+    private Integer page;
+
+    {
+        author = new Author();
+        page = 0;
+    }
+
     public Author getAuthor() {
-        author = authorEjbLocal.getAuthor(26L);
         return author;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public Author findAuthorById() {
+        author = authorEjbLocal.getAuthor(author.getId());
+        return author;
+    }
+
+    public List<Author> getAuthorsPage() {
+        Integer size = 10;
+        return authorEjbLocal.getAuthorsPage(new Pageable(size * page, size, Sort.ASC));
     }
 }
