093ced714a0918b2581bb0c4184321a0d958e46d
KirillPikov <pikov_kirya@bk.ru>
2020-01-20 17:07:42 +0400

    Update AuthorController.java

--- web/src/main/java/com/mediasoft/bookstore/web/AuthorController.java
+++ web/src/main/java/com/mediasoft/bookstore/web/AuthorController.java
@@ -2,12 +2,14 @@ package com.mediasoft.bookstore.web;
 
 import com.mediasoft.bookstore.author.ejb.AuthorEjbLocal;
 import com.mediasoft.bookstore.author.entity.Author;
+import com.mediasoft.bookstore.common.loggable.Loggable;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
+@Loggable
 @RequestScoped
 public class AuthorController {
 
