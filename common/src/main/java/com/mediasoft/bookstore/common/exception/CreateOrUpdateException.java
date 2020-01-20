a4286c70389eac2b96ecefeb3850d95a3ba4d3ad
KirillPikov <pikov_kirya@bk.ru>
2020-01-20 17:06:04 +0400

    Create CreateOrUpdateException.java

---
+++ common/src/main/java/com/mediasoft/bookstore/common/exception/CreateOrUpdateException.java
@@ -0,0 +1,8 @@
+package com.mediasoft.bookstore.common.exception;
+
+public class CreateOrUpdateException extends RuntimeException {
+
+    public CreateOrUpdateException(String message) {
+        super(message);
+    }
+}
